29cf8801a35154d6f38b54225a7f2d3c0271d7d0
RyosukeMizukami <mukusuke421@gmail.com>
2020-02-18 18:00:49 +0900

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #log_in @user
+      log_in @user
       #保存の成功をここで扱う。
       flash[:success] = "Welcome to the SNS App"
       redirect_to @user
